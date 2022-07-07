6a1d38c492bffdef90b3591cb1ff9b66331152c2
VneTraffiqua <safe72again@gmail.com>
2022-07-07 10:13:41 -0700

    Added env_path, .env

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
 import collections
+import os
+from dotenv import load_dotenv
+import argparse
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Разворачивает wine при запуске')
+    parser.add_argument('-e', '--env_path', help='Укажите путь к файлу конфигурации', default='.env')
+    env_path = parser.parse_args()
+    load_dotenv(env_path.env_path)
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-
     template = env.get_template('base.html')
-    excel_data_df = pandas.read_excel('wine.xlsx', sheet_name='Лист1', keep_default_na=False)
+    excel_data_df = pandas.read_excel(os.getenv('FILE_PATH'), sheet_name=os.getenv('SHEET_NAME'), keep_default_na=False)
     dict = excel_data_df.to_dict(orient='index')
     vine_list = [dict[dic] for dic in dict]
     grouped_vines = collections.defaultdict(list)
     [grouped_vines[vine['Категория']].append(vine) for vine in vine_list]
 
     rendered_page = template.render(
-        age=datetime.date.today().year - 1920,
+        age=datetime.date.today().year - int(os.getenv('WORKING_SINCE')),
         vines=grouped_vines
     )
 
